038363a09792f83fcf86e8979b34f178b4f8ef2f
张帅丹 <zhangshuaidan@wingbow.com.cn>
2019-08-15 15:22:09 +0800

    [2019-8-15] 处理渲染进程导入electron

--- .umirc.js
+++ .umirc.js
@@ -3,6 +3,8 @@
 
 
 // Instantiate the configuration with a new API
+import { join } from 'path';
+import slash from 'slash';
 
 export default {
   treeShaking: true,
@@ -39,4 +41,26 @@ export default {
     }
   },
   outputPath:'./app/render', // 更改输出目录
+  externals(context, request, callback) {
+    const isDev = process.env.NODE_ENV === 'development';
+    let isExternal = false;
+    const load = [
+      'electron',
+      'fs',
+      'path',
+      'os',
+      'url',
+      'child_process'
+    ];
+    if (load.includes(request)) {
+      isExternal = `require("${request}")`;
+    }
+    const appDeps = Object.keys(require('./app/package').dependencies);
+    if (appDeps.includes(request)) {
+      const orininalPath = slash(join(__dirname, './app/node_modules', request));
+      const requireAbsolute = `require('${orininalPath}')`;
+      isExternal = isDev ? requireAbsolute : `require('${request}')`;
+    }
+    callback(null, isExternal);
+  },
 }
